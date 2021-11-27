c2a0bec531b6445aa435988976cd9bbf518e8641
paisza1230 <73965756+paisza1230@users.noreply.github.com>
2021-11-27 22:59:01 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+
+filter_btns.forEach( (btn) =>{
+  btn.addEventListener("click",() => {
+    filter_btns.forEach((button) => button.classList.remove("active"));
+    btn.classList.add("active");
+    // ย้ายพื้นหลังของปุ่ม
+    let filterValue = btn.dataset.filter;
+    
+    $(".grid").isotope({filter: filterValue});
+
+    //เรียกโค้ดjavaมาจัดกลุ่มให้
+});
+});
+
+
+$(".grid").isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    transitionDuration: '0.6s',
+  });
+
+  
+//////////////////////////////////////
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
